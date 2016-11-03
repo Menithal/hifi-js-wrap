9edb7eb8df3f7e6844bb3fda360449b80f1c5608
Menithal <matti.lahtinen@norteclabs.com>
2016-11-03 12:27:37 +0200

    Added interactions

--- README.md
+++ README.md
@@ -52,20 +52,42 @@ Now, most of the time one isn't looking for an existing entity or overlay
 that already exists. You actually are creating them.
 
 Expanding on this, you may want to create a custom object that's not in the world
-
 ```
 var e = new Entity({properties})
 e.addEntity()
 ```
-
 In this, We create a new `Entity` bound to `entity` then we add this instance of
 `entity` to the world via `entity.addEntity()`
 
+# Basic Entity Interaction
 
+Basic Interactions for an entity can be bound with a single callback line
+```
+e.setInteractionStart(function(instance,event){ print("Interaction")})
+```
+This binds both hand controllers AND mouse to a single callback.
+The event being an [MouseEvent](https://github.com/highfidelity/hifi/blob/master/libraries/script-engine/src/MouseEvent.h)
+Instance is the Wrapper Entity, that allows you to manipulate the object.
 
-Some advanced stuff:
+For Example:
+```
+var hopper = new Entity({type:"Box", position:MyAvatar.position, name:"Hopper", color:{red:0,blue:0,green:255},dynamic: true, gravity: {x:0,y:-2,z:0}}).addEntity();
+hopper.setInteractionStart(function(instance, event){
+  instance.filter(["velocity"])
+          .editProperties({velocity: {x:0,z:0,y:5}})
+          .updateEntity();
+})
+```
 
-Would basically get an Entity with a uuid in world, get its latest position, edit this position
-one unit higher, and then update the info to the world.
+Similarly, interactions can be bound to `setInteractionHold` and `setInteractionStop`
 
-The filter method would simply make sure that for the period of the chain, we are only updating position
+When setInteraction is called, both the Mouse
+ AND hand controller interactions are bound to the object.
+
+to clear all interactions, simply call,
+
+```
+e.clearInteractions()
+```
+
+# Advanced Entity Interaction
--- hifi-js-wrap.js
+++ hifi-js-wrap.js
@@ -3,65 +3,44 @@ Hifi JS Wrap Library Prototype (USE AT YOUR OWN RISK)
 Matti 'Menithal' Lahtinen on 3/11/16.
 Work in Progress
 
-MIT License
-
-Copyright (c) 2016 Matti Lahtinen
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+Distributed under MIT License
+See LICENCE for License details.
 */
 
-
 function Entity(properties) {
-  if(typeof properties === "string"){
-    this.properties = Entities.getEntityProperties(id)
-    if(this.properties && this.properties !== null && this.properties.length !== 0){
-      this.id = properties.id
-    }else{
-      this.id = null
-      this.properties = {}
+    if (typeof properties === "string") {
+        this.properties = Entities.getEntityProperties(id)
+        if (this.properties && this.properties !== null && this.properties.length !== 0) {
+            this.id = properties.id
+        } else {
+            this.id = null
+            this.properties = {}
+        }
+    } else {
+        this.properties = properties
     }
-  }else{
-    this.properties = properties
-  }
+    this.callbacks = {}
+    this._filter = []
 }
 Entity.prototype = {
-    properties: {},
+    properties: null,
     id: null,
-    _filter: [],
+    _filter: null,
+    callbacks: null,
     filter: function(filter) {
-        if(this.id !== null){ // Lets not destroy properties if this isnt even in the world yet!
-          this._filter = filter
-          return this.sync(filter)
+        if (this.id !== null) { // Lets not destroy properties if this isnt even in the world yet!
+            this._filter = filter
+            return this.sync(filter)
         }
         return this
     },
     addEntity: function() {
-        if (this.id === null) {
-            this.id = Entities.addEntity(this.properties)
-        }
+        if (this.id === null) this.id = Entities.addEntity(this.properties)
         return this
     },
     deleteEntity: function() {
         try {
-            if (this.id !== null) {
-                Entities.deleteEntity(this.id)
-            }
+            if (this.id !== null) Entities.deleteEntity(this.id)
         } catch (e) {
             print("Entity does no longer exist.")
         }
@@ -70,43 +49,102 @@ Entity.prototype = {
     },
     sync: function(filter) {
         if (this.id !== null) {
-            filter = (filter === null || filter === undefined)? this._filter : filter
+            filter = (filter === null || filter === undefined) ? this._filter : filter
             var newProperties = Entities.getEntityProperties(this.id, filter)
             print(JSON.stringify(newProperties.length))
             if (newProperties.id !== undefined) {
                 this.properties = newProperties
-            }else{
-              print("FCC")
+            } else {
                 this.id = null
             }
         }
         this._filter = []
         return this
     },
-    getProperties: function(filters) { // Filtered properties update. We dont need to update everything always, so sometimes its good to define this.
-        filters = (filters === null || filters === undefined)? this._filter : filters
+    editProperties: function(newProperties) {
+        for (var property in newProperties){ this.properties[property] = newProperties[property] }
+        return this
+    },
+    callMethod: function(methodName, params) {
+        if (this.id !== null){ Entities.callEntityMethod(this.id, methodName, params) }
+        return this
+    },
+    unbind: function(method) {
+        if (this.callbacks[method] !== null){ return this }
+        Script.clearEventHandler(this.id, method, this.callbacks[method])
+        return this
+    },
+    bind: function(method, callback, override) {
+        if (override === undefined){ override = true }
+        if (this.callbacks[method] !== null){ this.unbind(method, this.callbacks[method]) }
+        var s = this
+        this.callbacks[method] = override ? function() {
+            callback(s, this.arguments)
+        } : callback
+        Script.addEventHandler(this.id, method, callback)
+        return this
+    },
+    clearInteractions: function() {
+        this.unbind("startNearTrigger")
+            .unbind("clickDownOnEntity")
+            .unbind("continueNearTrigger")
+            .unbind("holdingClickOnEntity")
+            .unbind("continueNearTrigger")
+            .unbind("holdingClickOnEntity")
 
+        return this
+    },
+    setInteractionStart: function(call) { // Creates Wraps mouse clicks and Trigger calls together.
+        var s = this
+        this.bind("startNearTrigger", function() {
+                call(s, {
+                    button: "hand"
+                })
+            }, false)
+            .bind("clickDownOnEntity", function(i, m) {
+                call(s, m)
+            }, false)
+        return this
+    },
+    setInteractionHold: function(call) { // Creates Wraps mouse clicks and Trigger calls together.
+        var s = this
+        this.bind("continueNearTrigger", function() {
+                call(s, {
+                    button: "hand"
+                })
+            }, false)
+            .bind("holdingClickOnEntity", function(i, m) {
+                call(s, m)
+            }, false)
+        return this
+    },
+    setInteractionStop: function(call) { // Creates Wraps mouse clicks and Trigger calls together.
+        var s = this
+        this.bind("stopNearTrigger", function() {
+                call(s, {
+                    button: "hand"
+                })
+            }, false)
+            .bind("clickReleaseOnEntity", function(i, m) {
+                call(s, m)
+            }, false)
+        return this
+    },
+    updateEntity: function() {
+        try {
+            if (this.id !== null) Entities.editEntity(this.id, this.getProperties())
+        } catch (e) {
+            print("EWrap: Entity does not exist in world.")
+        }
+        return this.sync()
+    },
+    getProperties: function(filters) { // Filtered properties update. We dont need to update everything always, so sometimes its good to define this.
+        filters = (filters === null || filters === undefined) ? this._filter : filters
         if (filters.length === 0) return this.properties
         var filtered = {}
         for (var index in filters) {
             filtered.push(this.properties[filters[index]])
         }
         return filtered
-    },
-    editProperties: function(newProperties) {
-        for (var property in newProperties) {
-            this.properties[property] = newProperties[property]
-        }
-        return this
-    },
-    updateEntity: function() {
-        try {
-            if (this.id !== null) {
-                Entities.editEntity(this.id, this.getProperties())
-            }
-        } catch (e) {
-            print("EWrap: Entity does not exist in world.")
-        }
-        return this.sync()
     }
 }
