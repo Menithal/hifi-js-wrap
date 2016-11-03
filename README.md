44ce5ebab4b5907d594986aeb845e931e0f7a90b
Menithal <matti.lahtinen@norteclabs.com>
2016-11-03 09:14:09 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# hifi-js-wrap
-High Fidelity Object Wrap
+# Wrapper for Entities and Overlays
+For [High Fidelity Metaverse's](https://highfidelity.io) JS-Scripting Language
+to work alike to JS OOP.
+
+## Note that this is still work in progress.
+
+Allows for JS Chain notation over the Default High Fidelity settings.
+
+In Laymans, This means that you can create and control an entity using just a JS Object with out calling "Entities".
+
+# Entity Manipulation
+
+```
+var e = new Entity("uuid")
+```
+Would bind a High Fidelity Entity into the JS variable `e`.
+`e.properties` would contain everything Hifi's `Entities.getEntityProperties`
+would return for the uuid from the last sync.
+
+Now `e.properties` does not currently stay uptodate automatically, its only
+last time a `sync` was called.
+
+By Default, calling the uuid constructor of `Entity` will call `sync`.
+
+To sync the entity instance later on in the code, we call
+```
+e.sync()
+```  
+this makes sure that the entity instance is up-to-date to the one in the world.
+
+Note that `sync`, `filter`, and all `*Entity` functions will always return the instance of the JS Object,
+so you can do some chaining, if you so wish:
+
+Forexample:
+
+```
+var anewVar = new Entity("uuid")
+anewVar.filter(["position"])
+       .editProperties({position: Vec3.sum({x:0,z:0,y:1},anewVar.properties.position)})
+       .updateEntity()
+```
+The above would first find uuid and set it to `anewVar`.
+it would then be connected to the instance.
+
+Calling `filter(["position"])` sets what we will update to just "position"
+edit the position one unit up, and then update it in world. the instance is
+then set to the anewVar
+
+# Entity creation
+
+Now, most of the time one isn't looking for an existing entity or overlay
+that already exists. You actually are creating them.
+
+Expanding on this, you may want to create a custom object that's not in the world
+
+```
+var e = new Entity({properties})
+e.addEntity()
+```
+
+In this, We create a new `Entity` bound to `entity` then we add this instance of
+`entity` to the world via `entity.addEntity()`
+
+
+
+Some advanced stuff:
+
+Would basically get an Entity with a uuid in world, get its latest position, edit this position
+one unit higher, and then update the info to the world.
+
+The filter method would simply make sure that for the period of the chain, we are only updating position
---
+++ hifi-js-wrap.js
@@ -0,0 +1,109 @@
+/**
+Hifi JS Wrap Library Prototype (USE AT YOUR OWN RISK)
+Matti 'Menithal' Lahtinen on 3/11/16.
+Work in Progress
+
+MIT License
+
+Copyright (c) 2016 Matti Lahtinen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+function Entity(properties) {
+  if(typeof properties === "string"){
+    this.properties = Entities.getEntityProperties(id)
+    if(this.properties && this.properties !== null && this.properties.length !== 0){
+      this.id = properties.id
+    }else{
+      this.id = null
+      this.properties = {}
+    }
+  }else{
+    this.properties = properties
+  }
+}
+Entity.prototype = {
+    properties: {},
+    id: null,
+    filter: [],
+    filter: function(filter) {
+        if(this.id !== null){ // Lets not destroy properties if this isnt even in the world yet!
+          this.filter = filter | []
+          return this.sync()
+        }
+        return this
+    },
+    addEntity: function() {
+        if (this.id === null) {
+            this.id = Entities.addEntity(this.properties)
+        }
+        return this
+    },
+    deleteEntity: function() {
+        try {
+            if (this.id !== null) {
+                Entities.deleteEntity(this.id)
+            }
+        } catch (e) {
+            print("Entity does no longer exist.")
+        }
+        this.id = null
+        return this
+    },
+    sync: function(filter) {
+        if (this.id !== null) {
+            filter = filter | this.filter
+            var newProperties = Entities.getEntityProperties(this.id, filter)
+            if (newProperties !== null && newProperties.length > 0) {
+                this.properties = newProperties
+            }else{
+              this.id = null
+            }
+        }
+        this.filter = []
+        return this
+    },
+    getProperties: function(filters) { // Filtered properties update. We dont need to update everything always, so sometimes its good to define this.
+        filter = filter | this.filter // Because apparently I cant define this above!
+        if (filters.length === 0) return this.properties
+        var filtered = {}
+        for (var index in filters) {
+            filtered.push(this.properties[filters[index]])
+        }
+        return filtered
+    },
+    editProperties: function(newProperties) {
+        for (var property in newProperties) {
+            this.properties[property] = newProperties[property]
+        }
+        return this
+    },
+    updateEntity: function() {
+        try {
+            if (this.id !== null) {
+                Entities.editEntity(this.id, this.getProperties())
+            }
+        } catch (e) {
+            print("EWrap: Entity does not exist in world.")
+        }
+        return this.sync()
+    }
+}
